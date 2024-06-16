5708326bad8932c0862f48cfba2d11c794aa69eb
tengku yasin habib <tengkuyasinhabib@gmail.com>
2024-06-16 21:12:43 +0700

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,21 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Yasin Habib's Portofolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "m0ekb5w3gz44",
+      "spaceId": "-Hs85Ol1lPFpICFa3Rh2Ckz_9XfPR3E9nGADCyXxEr8"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+};
+
+export default config;
